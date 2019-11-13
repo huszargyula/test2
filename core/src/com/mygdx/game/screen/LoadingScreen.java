a4016a50f65f0ab30abe5f1064d9cfe6ab928ec7
huszargyula <gyuland@gmail.com>
2019-11-13 20:36:20 +0100

    feketére átírtam a jobb láthatóság miatt

--- core/src/com/mygdx/game/screen/LoadingScreen.java
+++ core/src/com/mygdx/game/screen/LoadingScreen.java
@@ -36,7 +36,7 @@ public class LoadingScreen extends AbstractScreen
     @Override
     public void render(float delta) {
 
-        Gdx.gl.glClearColor(0,1,0,1);
+        Gdx.gl.glClearColor(0,0,0,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         //assetManager.updet= betülti, igaz ha befejeződött a betöltés
@@ -65,7 +65,6 @@ public class LoadingScreen extends AbstractScreen
     public void resume() {
 
     }
-
 
 
     @Override
